2ffcc05e2e271d6cae7fb9719b7edf8d1257c86a
WantedKing99 <85559678+WantedKing99@users.noreply.github.com>
2021-12-20 17:19:30 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require('express')
 const app = express()
 
 app.use(express.static("public"))
+// app.use(express.urlencoded ({ extended:true }));
 
 app.set('view engine', 'ejs')
-
-app.get('/', function (req, res) {
- res.render('index')
-})
 
 const gameRouter = require('./routes/game')
 
